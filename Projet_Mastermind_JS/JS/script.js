fc0b90e0580feff7f5f150fa78f935bac9673231
alexiszucher <48800960+alexiszucher@users.noreply.github.com>
2020-12-10 17:53:35 +0100

    Update script.js

--- Projet_Mastermind_JS/JS/script.js
+++ Projet_Mastermind_JS/JS/script.js
@@ -42,7 +42,7 @@ class Mastermind {
         
         var plateau = document.getElementById('plateau');
         for(var i=nbTentatives-1; i >= 0; i--) {
-            plateau.innerHTML += '<li> <div class="result" d-f="0"><b></b><b></b><b></b><b></b><em></em></div><div class="attempt"><i id="'+i+'1"></i><i id="'+i+'2"></i><i id="'+i+'3"></i><i id="'+i+'4"></i><tt></tt></div></li> ';
+            plateau.innerHTML += '<li> <div class="result" d-f="0"><b id="verif'+i+'1"></b><b id="verif'+i+'2"></b><b id="verif'+i+'3"></b><b id="verif'+i+'4"></b><em></em></div><div class="attempt"><i id="'+i+'1"></i><i id="'+i+'2"></i><i id="'+i+'3"></i><i id="'+i+'4"></i><tt></tt></div></li> ';
             this.casePourTenterDeTrouverLeCode[i] = [i+"1",i+"2",i+"3",i+"4"];
         }
     }
@@ -54,6 +54,21 @@ class Mastermind {
         for(var i = 1; i <= this.code.length; i++) {
             var code = document.getElementById("code"+i);
             code.style.backgroundColor = this.code[i-1];
+        }
+    }
+
+    checkLineTentative() {
+        var gagne = 0;
+        for(var i = 1; i <= this.code.length; i++) {
+            var colorCircle = document.getElementById(""+mastermind.positionTentativeCouleurX + ""+i);
+            if(colorCircle.style.backgroundColor == this.code[i-1]) {
+                var colorVerif = document.getElementById("verif"+mastermind.positionTentativeCouleurX+""+i);
+                colorVerif.style.backgroundColor = "black";
+                gagne++;
+            }
+        }
+        if(gagne == 4) {
+            alert("TU AS GAGNE !!!");
         }
     }
 }
@@ -69,6 +84,7 @@ function onClickTentativeColor(index) {
     colorCircle.style.backgroundColor = paletteColor[index];
     mastermind.positionTentativeCouleurY++;
     if(mastermind.positionTentativeCouleurY == 5) {
+        mastermind.checkLineTentative();
         mastermind.positionTentativeCouleurX++;
         mastermind.positionTentativeCouleurY = 1;
     }
